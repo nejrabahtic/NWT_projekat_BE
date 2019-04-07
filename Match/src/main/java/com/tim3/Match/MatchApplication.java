dea2d8bb6bee075c7e2e9da1c66baa4fc2825ea8
NejraBahtic <nejrabahtic@gmail.com>
2019-04-07 20:30:34 +0200

    Eureka server

--- Match/src/main/java/com/tim3/Match/MatchApplication.java
+++ Match/src/main/java/com/tim3/Match/MatchApplication.java
@@ -2,7 +2,9 @@ package com.tim3.Match;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MatchApplication {
 
